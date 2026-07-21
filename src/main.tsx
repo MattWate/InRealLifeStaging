1ca24c2435abb4eb1b414cc698e40ac416329f15
MattWate <matthew@dubyahinnovation.com>
2026-07-21 19:37:08 +0200

    Route Curiocity profile showcase

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import OperatorProfile from './OperatorProfile';
 import './styles.css';
+
+const isOperatorProfile = window.location.pathname === '/profiles/curiocity-green-point';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {isOperatorProfile ? <OperatorProfile /> : <App />}
     </BrowserRouter>
   </React.StrictMode>,
 );
