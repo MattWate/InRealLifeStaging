e400d5ba83cae5857f6767f96df65cb8d10974f3
MattWate <matthew@dubyahinnovation.com>
2026-07-22 20:35:29 +0200

    Route screening workspace

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import OperatorProfile from './OperatorProfile';
+import Screening from './Screening';
 import './styles.css';
 import './operator-profile-insights.css';
 
-const isOperatorProfile = window.location.pathname === '/profiles/curiocity-green-point';
+const path = window.location.pathname;
+const page = path.startsWith('/screening')
+  ? <Screening />
+  : path === '/profiles/curiocity-green-point'
+    ? <OperatorProfile />
+    : <App />;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {isOperatorProfile ? <OperatorProfile /> : <App />}
-    </BrowserRouter>
+    <BrowserRouter>{page}</BrowserRouter>
   </React.StrictMode>,
 );
